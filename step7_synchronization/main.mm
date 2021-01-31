c255821d420ef0433e2e0b7bcadd6d1d9827cff5
bodguy <alsrudgns99@gmail.com>
2021-01-31 12:19:04 +0900

    remove glfw dependency

--- step7_synchronization/main.mm
+++ step7_synchronization/main.mm
@@ -1,25 +1,36 @@
-#define GLFW_INCLUDE_NONE
-
-#import <GLFW/glfw3.h>
-
-#define GLFW_EXPOSE_NATIVE_COCOA
-
-#import <GLFW/glfw3native.h>
+#import <Cocoa/Cocoa.h>
 #import <Metal/Metal.h>
-#import <QuartzCore/QuartzCore.h>
-#import <simd/simd.h>
-#include <cassert>
-#include <cstdio>
+#import <QuartzCore/CAMetalLayer.h>
 #include <string>
-
-float deltaTime = 0.0f;    // 마지막 프레임과 현재 프레임 사이의 시간
-float lastFrame = 0.0f; // 마지막 프레임의 시간
+#include <cmath>
+#include <chrono>
 
 struct FragmentUniforms {
   FragmentUniforms(float _brightness) : brightness{_brightness} {}
 
   float brightness;
 };
+
+static const int k_WindowWidth = 800;
+static const int k_WindowHeight = 600;
+
+@class MetalView;
+MetalView *g_nsView;
+
+id <MTLDevice> g_mtlDevice;
+id <MTLCommandQueue> g_mtlCommandQueue;
+id <MTLRenderPipelineState> g_mtlPipelineState;
+id <MTLBuffer> g_vertexBuffer;
+id <MTLBuffer> g_uniformBuffer;
+static FragmentUniforms g_fragmentUniforms{1.f};
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+@property(strong, nonatomic) NSWindow *window;
+@end
+
+@interface MetalView : NSView
+@property(nonatomic, assign) CAMetalLayer *metalLayer;
+@end
 
 bool read_file(const std::string &filepath, std::string &out_source) {
     FILE *fp = nullptr;
@@ -37,65 +48,80 @@ bool read_file(const std::string &filepath, std::string &out_source) {
     return true;
 }
 
-static void error_callback(int error, const char *description) {
-    fputs(description, stderr);
+using high_resolution_clock = std::chrono::high_resolution_clock;
+using hr_time_point = std::chrono::time_point<high_resolution_clock>;
+using millisecond = std::chrono::duration<float, std::milli>;
+hr_time_point t1, t2;
+float elapsedTime = 0.f;
+
+void doRender() {
+    id <CAMetalDrawable> drawable = [g_nsView.metalLayer nextDrawable];
+
+    t1 = high_resolution_clock::now();
+    float deltaTime = std::chrono::duration_cast<millisecond>(t2 - t1).count() * 0.001;
+    t2 = t1;
+
+    // wait
+
+    // update logic
+    g_fragmentUniforms.brightness = 0.5f * std::cos(elapsedTime) + 0.5f;
+    memcpy([g_uniformBuffer contents], &g_fragmentUniforms, sizeof(FragmentUniforms));
+    elapsedTime += deltaTime;
+
+    id <MTLCommandBuffer> commandBuffer = [g_mtlCommandQueue commandBuffer];
+    MTLRenderPassDescriptor *passDescriptor = [MTLRenderPassDescriptor renderPassDescriptor];
+    id <MTLTexture> framebufferTexture = drawable.texture;
+    passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 0.0);
+    passDescriptor.colorAttachments[0].texture = framebufferTexture;
+    passDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
+    passDescriptor.colorAttachments[0].storeAction = MTLStoreActionStore;
+
+    id <MTLRenderCommandEncoder> commandEncoder = [commandBuffer renderCommandEncoderWithDescriptor:passDescriptor];
+    [commandEncoder setRenderPipelineState:g_mtlPipelineState];
+    [commandEncoder setVertexBuffer:g_vertexBuffer offset:0 atIndex:0];
+    [commandEncoder setFragmentBuffer:g_uniformBuffer offset:0 atIndex:0];
+    [commandEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:6];
+    [commandEncoder endEncoding];
+    [commandBuffer presentDrawable:drawable];
+    // signal
+
+    [commandBuffer commit];
 }
 
-static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
-int main(void) {
-    id <MTLDevice> device = MTLCreateSystemDefaultDevice();
-    if (!device) exit(EXIT_FAILURE);
-    CAMetalLayer *layer = [CAMetalLayer layer];
-    layer.device = device;
-    layer.pixelFormat = MTLPixelFormatBGRA8Unorm;
-    layer.opaque = YES;
-
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit()) exit(EXIT_FAILURE);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow *window = glfwCreateWindow(640, 480, "Metal Example", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-    NSWindow *nswin = glfwGetCocoaWindow(window);
-    nswin.contentView.layer = layer;
-    nswin.contentView.wantsLayer = YES;
+int init() {
+    g_mtlDevice = MTLCreateSystemDefaultDevice();
+    g_mtlCommandQueue = [g_mtlDevice newCommandQueue];
 
     MTLCompileOptions *compileOptions = [MTLCompileOptions new];
     compileOptions.languageVersion = MTLLanguageVersion1_1;
-    NSError *compileError;
+    NSError *compileError = nullptr;
     std::string shaderSource;
     if (!read_file("../basic.metal", shaderSource)) {
-        fprintf(stderr, "Shader not found");
+        NSLog(@"Shader not found");
         return EXIT_FAILURE;
     }
-    id <MTLLibrary> lib = [device newLibraryWithSource:[NSString stringWithFormat:@"%s", shaderSource.c_str()] options:compileOptions error:&compileError];
-    if (!lib) {
+    id <MTLLibrary> library = [g_mtlDevice newLibraryWithSource:[NSString stringWithFormat:@"%s", shaderSource.c_str()] options:compileOptions error:&compileError];
+    if (!library) {
         NSLog(@"can't create library: %@", compileError);
-        glfwTerminate();
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
-    id <MTLFunction> vs = [lib newFunctionWithName:@"main0"];
-    assert(vs);
-    id <MTLFunction> fs = [lib newFunctionWithName:@"main1"];
-    assert(fs);
-    id <MTLCommandQueue> commandQueue = [device newCommandQueue];
-    assert(commandQueue);
-    MTLRenderPipelineDescriptor *rpd = [MTLRenderPipelineDescriptor new];
-    rpd.vertexFunction = vs;
-    rpd.fragmentFunction = fs;
-    rpd.colorAttachments[0].pixelFormat = layer.pixelFormat;
+    [compileOptions release];
 
-    NSError *error = NULL;
+    MTLRenderPipelineDescriptor *pipelineDescriptor = [MTLRenderPipelineDescriptor new];
+    pipelineDescriptor.vertexFunction = [library newFunctionWithName:@"main0"];
+    pipelineDescriptor.fragmentFunction = [library newFunctionWithName:@"main1"];
+    pipelineDescriptor.colorAttachments[0].pixelFormat = MTLPixelFormatBGRA8Unorm;
+    [library release];
+
+    NSError *error = nullptr;
     MTLPipelineOption option = MTLPipelineOptionBufferTypeInfo | MTLPipelineOptionArgumentInfo;
     MTLRenderPipelineReflection *reflectionObj;
-    id <MTLRenderPipelineState> renderPipelineState = [device newRenderPipelineStateWithDescriptor:rpd options:option reflection:&reflectionObj error:&error];
-    assert(renderPipelineState);
+    g_mtlPipelineState = [g_mtlDevice newRenderPipelineStateWithDescriptor:pipelineDescriptor options:option reflection:&reflectionObj error:&error];
+    if (!g_mtlPipelineState) {
+        NSLog(@"Failed to create render pipeline state: %@", error);
+        return EXIT_FAILURE;
+    }
+    [pipelineDescriptor release];
 
     for (MTLArgument *arg in reflectionObj.vertexArguments) {
         NSLog(@"Found arg: %@\n", arg.name);
@@ -119,8 +145,6 @@ int main(void) {
         }
     }
 
-    glfwSetKeyCallback(window, key_callback);
-
     float quadVertexData[] = {
             0.5, -0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0,
             -0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0,
@@ -130,50 +154,150 @@ int main(void) {
             0.5, -0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0,
             -0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
     };
-    id <MTLBuffer> vertexBuffer = [device newBufferWithBytes:quadVertexData length:sizeof(quadVertexData) options:MTLResourceOptionCPUCacheModeDefault];
-    FragmentUniforms fragmentUniforms = FragmentUniforms{1.f};
-    id <MTLBuffer> uniformBuffer = [device newBufferWithBytes:&fragmentUniforms length:sizeof(FragmentUniforms) options:MTLResourceOptionCPUCacheModeDefault];
+    g_vertexBuffer = [g_mtlDevice newBufferWithBytes:quadVertexData length:sizeof(quadVertexData) options:MTLResourceOptionCPUCacheModeDefault];
+    g_uniformBuffer = [g_mtlDevice newBufferWithBytes:&g_fragmentUniforms length:sizeof(FragmentUniforms) options:MTLResourceOptionCPUCacheModeDefault];
 
-    float elapsedTime = 0.f;
-    while (!glfwWindowShouldClose(window)) {
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        layer.drawableSize = CGSizeMake(width, height);
-        id <CAMetalDrawable> drawable = [layer nextDrawable];
-        assert(drawable);
-
-        float currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
-
-        // wait
-
-        // update logic
-        fragmentUniforms.brightness = 0.5f * std::cos(elapsedTime) + 0.5f;
-        memcpy([uniformBuffer contents], &fragmentUniforms, sizeof(FragmentUniforms));
-        elapsedTime += deltaTime;
-
-        // render logic
-        id <MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
-        MTLRenderPassDescriptor *renderPassDescriptor = [MTLRenderPassDescriptor new];
-        MTLRenderPassColorAttachmentDescriptor *cd = renderPassDescriptor.colorAttachments[0];
-        cd.texture = drawable.texture;
-        cd.loadAction = MTLLoadActionClear;
-        cd.clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 0.0);
-        cd.storeAction = MTLStoreActionStore;
-        id <MTLRenderCommandEncoder> commandEncoder = [commandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
-        [commandEncoder setRenderPipelineState:renderPipelineState];
-        [commandEncoder setVertexBuffer:vertexBuffer offset:0 atIndex:0];
-        [commandEncoder setFragmentBuffer:uniformBuffer offset:0 atIndex:0];
-        [commandEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:6];
-        [commandEncoder endEncoding];
-        [commandBuffer presentDrawable:drawable];
-        // signal
-
-        [commandBuffer commit];
-        glfwPollEvents();
-    }
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
+
+void renderDestroy() {
+    [g_uniformBuffer release];
+    [g_vertexBuffer release];
+    [g_mtlPipelineState release];
+    [g_mtlCommandQueue release];
+    [g_mtlDevice release];
+}
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+    if (init() != EXIT_SUCCESS) {
+        return EXIT_FAILURE;
+    }
+
+    NSApplication *application = [NSApplication sharedApplication];
+
+    AppDelegate *applicationDelegate = [[[AppDelegate alloc] init] autorelease];
+    [application setActivationPolicy:NSApplicationActivationPolicyRegular];
+    [application activateIgnoringOtherApps:YES];
+    [application setDelegate:applicationDelegate];
+    [application run];
+    [pool drain];
+
+    return EXIT_SUCCESS;
+}
+
+CVDisplayLinkRef g_displayLink;
+
+static CVReturn displayLinkCallback(
+        CVDisplayLinkRef displayLink,
+        const CVTimeStamp *now,
+        const CVTimeStamp *outputTime,
+        CVOptionFlags flagsIn,
+        CVOptionFlags *flagsOut,
+        void *displayLinkContext) {
+    doRender();
+    return kCVReturnSuccess;
+}
+
+@interface MyNSWindow : NSWindow
+- (BOOL)canBecomeMainWindow;
+
+- (BOOL)canBecomeKeyWindow;
+
+- (BOOL)acceptsFirstResponder;
+
+- (void)keyDown:(NSEvent *)anEvent;
+@end
+
+@implementation MyNSWindow
+- (BOOL)canBecomeMainWindow { return YES; }
+
+- (BOOL)canBecomeKeyWindow { return YES; }
+
+- (BOOL)acceptsFirstResponder { return YES; }
+
+- (void)keyDown:(NSEvent *)anEvent {
+    unsigned short keyCode = [anEvent keyCode];
+    printf("Key code: %d\n", keyCode);
+    if (keyCode == 53 || keyCode == 49) {
+        [self close];
+    }
+}
+@end
+
+@implementation AppDelegate
+- (id)init {
+    if (self = [super init]) {}
+    return self;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+    NSRect contentSize = NSMakeRect(0.0f, 0.0f, k_WindowWidth, k_WindowHeight);
+    g_nsView = [[MetalView alloc] initWithFrame:contentSize];
+
+    NSWindowStyleMask style = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable;
+    self.window = [[MyNSWindow alloc] initWithContentRect:contentSize
+                                                styleMask:style
+                                                  backing:NSBackingStoreBuffered
+                                                    defer:YES];
+    [self.window setTitle:@"Metal C++ Example6"];
+    [self.window setOpaque:YES];
+    [self.window setContentView:g_nsView];
+    [self.window makeMainWindow];
+    [self.window makeKeyAndOrderFront:nil];
+    [self.window makeFirstResponder:nil];
+
+    [self.window makeKeyAndOrderFront:self];
+
+    [g_nsView awakeFromNib];
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
+    return true;
+}
+@end
+
+@implementation MetalView
++ (id)layerClass {
+    return [CAMetalLayer class];
+}
+
+- (CALayer *)makeBackingLayer {
+    CAMetalLayer *backingLayer = [CAMetalLayer layer];
+    self.metalLayer = backingLayer;
+    return self.metalLayer;
+}
+
+- (instancetype)initWithFrame:(CGRect)frameRect {
+    if ((self = [super initWithFrame:frameRect])) {
+        self.wantsLayer = YES; // Providing Metal layer through makeBackingLayer
+
+        if (!g_mtlDevice) {
+            fprintf(stderr, "MetalView: ERROR - Metal device has not be created.\n");
+        }
+
+        self.metalLayer.device = g_mtlDevice;
+        self.metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    // As the last thing we do, fire up the display link timer for rendering.
+    // Create callback for rendering purposes. Need to check thread ids. I
+    // presume this CVDisplayLink callback is issued from the CFRunLoop
+    // generated by issuing a call to [NSApp run].
+    CVDisplayLinkCreateWithActiveCGDisplays(&g_displayLink);
+    CVDisplayLinkSetOutputCallback(g_displayLink, &displayLinkCallback, 0);
+    CVDisplayLinkSetCurrentCGDisplay(g_displayLink, 0);
+    CVDisplayLinkStart(g_displayLink);
+}
+
+- (void)dealloc {
+    CVDisplayLinkStop(g_displayLink);
+    CVDisplayLinkRelease(g_displayLink);
+    renderDestroy();
+    [super dealloc];
+}
+@end
